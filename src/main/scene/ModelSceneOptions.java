ed5f3afe153e2c66ae43befd6aee8427c4c0c387
Supremas1 <a.petrov.045@mail.ru>
2023-12-23 12:09:11 +0300

    load scene

---
+++ src/main/scene/ModelSceneOptions.java
@@ -0,0 +1,13 @@
+package main.scene;
+
+import javafx.scene.image.Image;
+//JavaFX 17.0.9
+public class ModelSceneOptions {
+    public boolean usedPolygonalGrid;
+    public boolean usedTexture;
+    public boolean usedLighting;
+
+    public String textureFileName = "";
+
+    public Image texture;
+}
