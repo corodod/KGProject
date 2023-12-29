2cc668e7086207647748139ec3ac534a8ebfb718
Supremas1 <a.petrov.045@mail.ru>
2023-12-29 13:05:57 +0300

    Deploy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,6 @@ module com.example.kgproject {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-//    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens com.cgvsu.kgproject to javafx.fxml;
